4c7b77fd3997728224c6f0efbd7d8cc5c8f01454
Jona <jonathasmarques70@gmail.com>
2025-11-11 15:22:38 -0400

    Remove duplicate input check for cake preference

--- if_else.py
+++ if_else.py
@@ -7,11 +7,4 @@ if verificacao == "sim":
 
 else:
     print('poxa, que pena! Eu gosto de bolo.')
-
-verificacao = input ('Você gosta de bolo? ')
-
-if verificacao == "sim":
-    print('Que bom! Eu também gosto de bolo.')
     
-else:
-    print('poxa, que pena! Eu gosto de bolo.')
